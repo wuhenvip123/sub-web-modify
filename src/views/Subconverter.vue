01481f99525d09dbecd7247e8899401a9d970c87
wuhenvip123 <160570053+wuhenvip123@users.noreply.github.com>
2025-07-03 20:08:41 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -28,7 +28,7 @@
               </el-form-item>
               <el-form-item label="后端地址:">
                 <el-select v-model="form.customBackend" allow-create filterable @change="selectChanged"
-                  placeholder="http://v.19910427.xyz" style="width: 100%">
+                  placeholder="httpS://hd.910527.xyz" style="width: 100%">
                   <el-option v-for="(v, k) in options.customBackend" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
